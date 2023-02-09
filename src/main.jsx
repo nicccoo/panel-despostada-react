8aa9ecc5e389bbe9eff2543be43b829f2d9a2b4f
nicccoo <nicolas_31_07@hotmail.com>
2023-02-09 08:35:54 -0300

    feat: changes merged

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/pages/ProduccionPage.jsx
+++ src/pages/ProduccionPage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import GraficoProduccionTotal from "../components/GraficoProduccionTotal/GraficoProduccionTotal";
 import GraficoEvolucionProd from "../components/GraficoEvolucionProd/GraficoEvolucionProd";
-<<<<<<< HEAD
-import ProductividadNeta from "../components/ProductividadNeta/ProductividadNeta";
-=======
 import Card from "../components/Card/Card";
 import CardData from "../components/Card/CardData";
->>>>>>> 540034b8df3db5fd7226060368dd7f1f0b5bf89f
 import "../styles.css";
 
 const ProduccionPage = () => {
@@ -14,11 +10,7 @@ const ProduccionPage = () => {
     <div className="main">
       <div className="aside-charts">
         <GraficoProduccionTotal />
-<<<<<<< HEAD
-        <ProductividadNeta />
-=======
         <Card details={CardData} />
->>>>>>> 540034b8df3db5fd7226060368dd7f1f0b5bf89f
       </div>
       <div className="main-charts">
         <GraficoEvolucionProd />
