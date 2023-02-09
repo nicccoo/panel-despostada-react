7ce8eed4235a569eb99ba9689e5e85393016962f
nicccoo <nicolas_31_07@hotmail.com>
2023-02-09 15:07:05 -0300

    feat: app routed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./header.css";
 import logo from "../../assets/logo.jpg";
+import reloj from "../../assets/ddd.png"
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-izq">
-        <Link to={"/"} exact>
+        <Link to={"/"} exact >
           <img className="header-izq__logo" src={logo} alt="" />
         </Link>
         <div className="header-izq__title">
@@ -15,10 +16,23 @@ const Header = () => {
           <h3 className="desc">Presencialidad (%): 95%</h3>
         </div>
       </div>
-      <div className="header-der">
+      <div className="header-center">
         <h3 className="desc">SALA DEPOSTADA TURNO MAÑANA</h3>
-        <span className="header-der__reloj">00:30</span>
+        <img src={reloj} alt="reloj" className="header-der__reloj" />
         <p>TIEMPO PARA PROXIMO DESCANSO</p>
+      </div>
+      <div className="header-der">
+        <nav className="header-der__navbar">
+        <Link to={"/"}>
+            PRODUCCION
+          </Link>        
+          <Link to={"/rechazos"}>
+            RECHAZOS
+          </Link>
+          <Link to={"/rechazos-resumen"}>
+            RESUMEN RECHAZOS
+          </Link>
+          </nav>
       </div>
     </div>
   );
