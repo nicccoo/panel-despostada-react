368b9078ccf36983c85ec08f74756c0cdd9f9dd5
nicccoo <nicolas_31_07@hotmail.com>
2023-02-08 21:37:52 -0300

    feat: produccion total chart added

--- src/components/GraficoProduccionTotal/GraficoProduccionTotal.jsx
+++ src/components/GraficoProduccionTotal/GraficoProduccionTotal.jsx
@@ -1,100 +1,105 @@
 import {
-    Chart as ChartsJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from "chart.js";
-  import { Bar } from "react-chartjs-2";
-  import { useState, useEffect } from "react";
-  import { CHART_COLOR_BLUE, CHART_COLOR_GREEN } from '../../utils/CHARTS_COLORS'
-  import './GraficoProduccionTotal.css';
+  Chart as ChartsJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { useState, useEffect } from "react";
+import { CHART_COLOR_BLUE, CHART_COLOR_GREEN } from "../../utils/CHARTS_COLORS";
+import "./GraficoProduccionTotal.css";
 
 ChartsJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  const GraficoProduccionTotal = () => {
+const GraficoProduccionTotal = () => {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
 
-    const [chartData, setChartData] = useState({
-        datasets: [],
-      });
-    
-      const [chartOptions, setChartOptions] = useState({});
-    
-      useEffect(() => {
-        setChartData({
-          labels: ['Producciones'],
-          datasets: [{
-               label: "Producción Real",
-               fontColor: "",
-               backgroundColor: CHART_COLOR_BLUE,
-               data: [1910],
-          }, {
-               label: "Producción Programada",
-               color: "#e6e9ec",
-               backgroundColor: CHART_COLOR_GREEN,
-               data: [2355],        
-          }]
-        });
-    
-        setChartOptions({
-          responsive: true,
-          layout: {
-            padding: 10,
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ["Producciones"],
+      datasets: [
+        {
+          label: "Producción Real",
+          fontColor: "",
+          backgroundColor: CHART_COLOR_BLUE,
+          data: [1910],
         },
-          indexAxis: "y",
-          scales: {
-            x: {
-              ticks: {
-                color: "#e6e9ec",              
-              },
-              grid: {               
-                  color: "#e6e9ec",              
-              },
-            },         
-            y: {
-              ticks: {
-                color: "#e6e9ec",                
-              }    ,
-              grid: {               
-                color: "#e6e9ec",              
-            }
-            },
-            beginAtZero: true,
+        {
+          label: "Producción Programada",
+          color: "#e6e9ec",
+          backgroundColor: CHART_COLOR_GREEN,
+          data: [2355],
+        },
+      ],
+    });
+
+    setChartOptions({
+      responsive: true,
+      layout: {
+        padding: 10,
+      },
+      indexAxis: "y",
+      scales: {
+        x: {
+          ticks: {
+            color: "#e6e9ec",
           },
-          plugins: {
-            legend: {              
-              position: "top",
-            },
-            labels: {
-              fontColor: "#e6e9ec"
-            },
-            title: {
-              display: true,
-              text: "PRODUCCIÓN TOTAL",
-              color: "#e6e9ec",
-              font: {
-                size: 15
-              }            
-            },
+          grid: {
+            color: "#e6e9ec",
           },
-        });
-      }, []);
+        },
+        y: {
+          ticks: {
+            color: "#e6e9ec",
+          },
+          grid: {
+            color: "#e6e9ec",
+          },
+        },
+        beginAtZero: true,
+      },
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        labels: {
+          fontColor: "#e6e9ec",
+        },
+        title: {
+          display: true,
+          text: "PRODUCCIÓN TOTAL",
+          color: "#e6e9ec",
+          font: {
+            size: 15,
+          },
+        },
+      },
+    });
+  }, []);
 
-      return ( 
-        <div className="aside-charts">
-            <Bar className="prod-total__barchart" data={chartData} options={chartOptions} />
-        </div>
-      )
+  return (
+    <div className="aside-charts">
+      <Bar
+        className="prod-total__barchart"
+        data={chartData}
+        options={chartOptions}
+      />
+    </div>
+  );
+};
 
-}
-
-export default GraficoProduccionTotal
+export default GraficoProduccionTotal;
