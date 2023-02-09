6f143ecaba9ee541041d506c4d566669ff817513
nicccoo <nicolas_31_07@hotmail.com>
2023-02-09 15:08:16 -0300

    feat: auto-navigation

--- src/pages/ProduccionPage.jsx
+++ src/pages/ProduccionPage.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import GraficoProduccionTotal from "../components/GraficoProduccionTotal/GraficoProduccionTotal";
 import GraficoEvolucionProd from "../components/GraficoEvolucionProd/GraficoEvolucionProd";
 import Card from "../components/Card/Card";
 import CardData from "../components/Card/CardData";
 import "../styles.css";
+import { useNavigate } from "react-router-dom";
 
 const ProduccionPage = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {    
+       return navigate("/rechazos");
+    }, 100000);
+  
+    return () => clearTimeout(timeout);
+  },[])
+
+
   return (
     <div className="main">
       <div className="aside-charts">
