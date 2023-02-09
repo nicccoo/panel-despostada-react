0dcf7da17a2866b3d5363acb9283ee4eb6cbf1bc
nicccoo <nicolas_31_07@hotmail.com>
2023-02-08 21:38:20 -0300

    feat: dynamic card rejection added

--- src/pages/ProduccionPage.jsx
+++ src/pages/ProduccionPage.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import GraficoProduccionTotal from '../components/GraficoProduccionTotal/GraficoProduccionTotal'
-import GraficoEvolucionProd from '../components/GraficoEvolucionProd/GraficoEvolucionProd'
-import ProductividadNeta from '../components/ProductividadNeta/ProductividadNeta'
+import React from "react";
+import GraficoProduccionTotal from "../components/GraficoProduccionTotal/GraficoProduccionTotal";
+import GraficoEvolucionProd from "../components/GraficoEvolucionProd/GraficoEvolucionProd";
+import Card from "../components/Card/Card";
+import CardData from "../components/Card/CardData";
 import "../styles.css";
 
 const ProduccionPage = () => {
   return (
     <div className="main">
-    <div className="aside-charts">
-      <GraficoProduccionTotal />
-      <ProductividadNeta />
+      <div className="aside-charts">
+        <GraficoProduccionTotal />
+        <Card details={CardData} />
+      </div>
+      <div className="main-charts">
+        <GraficoEvolucionProd />
+      </div>
     </div>
-    <div className="main-charts">
-     <GraficoEvolucionProd />        
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ProduccionPage
+export default ProduccionPage;
